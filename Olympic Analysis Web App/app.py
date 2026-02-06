ed9701d6cbd88808e15e013565224f3f5a561aec
ShreyashMundokar <143119514+ShreyashMundokar@users.noreply.github.com>
2026-02-06 17:38:44 +0530

    Add files via upload

---
+++ Olympic Analysis Web App/app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import preprocessor,helper
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv('athlete_events.csv')
+region_df=pd.read_csv('noc_regions.csv')
+
+df=preprocessor.preprocess(df, region_df)
+
+st.sidebar.title("Olympics Analysis")
+
+user_menu = st.sidebar.radio(
+    'Select an Option',
+    ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete-wise Analysis')
+)
+
+if user_menu=='Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years, country = helper.country_year_list(df)
+    selected_year = st.sidebar.selectbox("Select Year" , years)
+    selected_country = st.sidebar.selectbox("Select Country", country)
+
+    medal_tally=helper.fetch_medal_tally(df,  selected_year, selected_country)
+    if selected_year == 'Overall' and selected_country == 'Overall':
+        st.title('Overall Tally')
+
+    elif selected_year != 'Overall' and selected_country == 'Overall':
+        st.title("Medal Tally in " + str(selected_year) + " Olympics")
+
+    elif selected_year == 'Overall' and selected_country != 'Overall':
+        st.title(selected_country + " Overall Performance")
+    
+    elif selected_year != 'Overall' and selected_country != 'Overall':
+        st.title(selected_country + " performance in " + str(selected_year) + " Olympics")
+
+    st.table(medal_tally)
+
+if user_menu == 'Overall Analysis':
+    editions  = df['Year'].unique().shape[0]-1
+    cities  = df['City'].unique().shape[0]
+    sports  = df['Sport'].unique().shape[0]
+    events  = df['Event'].unique().shape[0]
+    athletes  = df['Name'].unique().shape[0]
+    nations  = df['region'].unique().shape[0]
+
+    st.title('Top Statistics')
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Editions")
+        st.title(editions)
+
+    with col2:
+        st.header('Hosts')
+        st.title(cities)
+
+    with col3:
+        st.header('Sports')
+        st.title(sports)
+
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Events")
+        st.title(events)
+
+    with col2:
+        st.header('Nations')
+        st.title(nations)
+
+    with col3:
+        st.header('Athletes')
+        st.title(athletes)
+
+
+    st.title("No. of Events over time(Every sport)")
+    fig, ax = plt.subplots(figsize=(20,20))
+    x=df.drop_duplicates(['Year', 'Sport', 'Event'])
+    sns.heatmap(x.pivot_table(index='Sport', columns='Year', values='Event', aggfunc='count').fillna(0).astype('int'), annot=True)
+
+    st.pyplot(fig)
+
+# st.dataframe(df)
